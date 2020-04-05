3689481cccd7a058238ebb3988c9a8658b6e4c4e
orysiayashchun <54995877+orysiayashchun@users.noreply.github.com>
2020-04-05 23:51:11 +0300

    Added 2 Testcases

---
+++ SeleniumTest/Tests/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+namespace SeleniumTest.Tests
+
+{
+    [TestFixture]
+    public class TestCase
+    {
+        private readonly IWebDriver driver;
+        private static readonly string url = "http://automationpractice.com/index.php";
+        private static readonly TimeSpan implisitWait = TimeSpan.FromMilliseconds(3000);
+        private WebDriverWait wait;
+        public TestCase()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(3000);
+        }
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => driver.Quit();//видаляє exe
+        [TestCase(true, "Blouse")]
+        [TestCase(false, "Blouce")]
+        public void Test(bool isPositive, string text)
+        {
+            driver.FindElement(By.Id("search_query_top")).Click();
+            driver.FindElement(By.Id("search_query_top")).SendKeys(text);
+            driver.FindElement(By.Id("search_query_top")).SendKeys(Keys.Enter);
+            driver.FindElement(By.Id("search_query_top")).Clear();
+            wait = new WebDriverWait(driver,TimeSpan.FromMilliseconds(750));
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            //wait.Until(x=>x.FindElements(By.));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.Zero;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+   
+}
